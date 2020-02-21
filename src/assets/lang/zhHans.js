e643d309bcb67c644b8bf2e31351111d3e255da6
Coceki <43731752+Coceki@users.noreply.github.com>
2020-02-21 20:59:34 +0800

    新增：序列播放哒呦

--- src/assets/lang/zhHans.js
+++ src/assets/lang/zhHans.js
@@ -2,6 +2,15 @@ module.exports = {
     ui: {
         title: "Peko按钮",
         mediacontorl: "控制按钮",
-        stopplay: "停止播放"
+        stopplay: "停止播放",
+        helptitle: "这咋玩啊peko?",
+        helpcontent: "点击按钮，就可以播放Pekora的语录哒呦~不需要等第一个播放完就可以直接点击下一个，也可以连点同一个按钮实现非常鬼畜的回音效果peko~",
+        additionalhelp: "还可以试试“序列播放”,可以全自动将短语合并生成一段话。详细操作请参考“序列播放”的操作说明peko~",
+        openorderplaymode: "开启序列播放",
+        orderplaymode: "序列播放",
+        orderlistnow: "当前的播放序列",
+        playthislist: "播放序列",
+        listempty: "你的序列还是空的哦peko~",
+        resetorder: "清空当前序列"
     }
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,79 @@
 <template>
   <v-container>
-    <!-- <v-row>
-        <div class="text-center">
-            <p>{{$t("ui.mediacontorl")}}</p>
-            <v-btn raised @click="stopPlay">{{$t("ui.stopplay")}}</v-btn>
-        </div>
-    </v-row> -->
+    <v-fab-transition>
+      <v-btn
+        class="mb-11"
+        v-show="orderplaymode"
+        color="primary"
+        dark
+        fixed
+        bottom
+        right
+        fab
+        @click="orderdialog=true"
+      >
+        <v-icon>mdi-reorder-horizontal</v-icon>
+      </v-btn>
+    </v-fab-transition>
+    <!---标题--->
+    <v-row align="center" justify="center">
+      <div class="text-center display-1 font-weight-bold">{{$t("ui.title")}}</div>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-switch v-model="orderplaymode" inset color="secondary" :label="$t('ui.openorderplaymode')"></v-switch>
+    </v-row>
+    <!---帮助文本--->
+    <v-row>
+      <v-col cols="12">
+        <v-card class="pa-4">
+          <p class="headline font-weight-blod">
+            <v-icon large color="primary">mdi-help-circle</v-icon>
+            {{$t("ui.helptitle")}}
+          </p>
+          <p>{{$t("ui.helpcontent")}}</p>
+          <p>{{$t("ui.additionalhelp")}}</p>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!---展示按钮--->
     <v-row v-for="group in voices" :key="group">
       <v-col cols="12">
         <v-card>
           <v-card-title>{{group.translation.Chinese}}</v-card-title>
           <v-container>
             <v-row no-gutters>
-            
-              <v-btn class="ma-2 pa-1" v-for="voice in group.voicelist" :key="voice.name" raised color="secondary" @click="play(voice)">{{voice.translation.Chinese}}</v-btn>
+              <v-btn
+                class="ma-2 pa-1"
+                v-for="voice in group.voicelist"
+                :key="voice.name"
+                raised
+                color="secondary"
+                @click="play(voice)"
+              >{{voice.translation.Chinese}}</v-btn>
             </v-row>
           </v-container>
         </v-card>
       </v-col>
     </v-row>
+    <!--序列播放-->
+    <v-dialog v-model="orderdialog" persistent>
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click="orderdialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{$t("ui.orderplaymode")}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-card class="pa-5">
+        <p class="title font-weight-blod">{{$t("ui.orderlistnow")}}</p>
+        <v-chip v-for="(selected,index) in orderlist" :key="selected" class="ma-2" close color="secondary" text-color="white" @click:close="deletelist(index)" @click="playOnly(selected)">{{selected.translation.Chinese}}}</v-chip>
+        <v-card-actions v-if="orderlist.length>0">
+          <v-btn raised color="primary" @click="orderplay">{{$t("ui.playthislist")}}</v-btn>
+          <v-btn text color="red" @click="resetorder">{{$t("ui.resetorder")}}</v-btn>
+        </v-card-actions> 
+        <p v-else>{{$t("ui.listempty")}}</p>     
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -26,17 +81,53 @@
 import voicelist from "../assets/voices.json";
 export default {
   data: () => ({
-    voices: voicelist.groups
+    voices: voicelist.groups,
+    orderplaymode: false,
+    orderlist: [],
+    orderdialog: false
   }),
   created() {
-    window.console.log(this.voices);
+    window.console.log(this.voices); //装载语音包path
   },
   methods: {
     play(item) {
+      if (this.orderplaymode) {
+        //判断序列播放
+        this.orderlist.push(item);
+        window.console.log(this.orderlist);
+      }
       let audio = new Audio("voices/" + item.path);
       this.voice = item;
       audio.play();
     },
+    playOnly(item){
+      let audio = new Audio("voices/" + item.path);
+      this.voice = item;
+      audio.play();
+    },
+    deletelist(i){//删除序列中的一个值
+      this.orderlist.splice(i,1);
+    },
+    orderplay(){
+      var audio = new Audio(); 
+      var i = 0;
+      let arry = this.orderlist
+      audio.preload = true;
+      audio.loop = false;
+      audio.src = "voices/"+arry[i].path;
+      audio.play();
+      audio.addEventListener('ended', playEndedHandler, false); 
+      function playEndedHandler(){
+        i++;
+        if(i <= arry.length){
+          audio.src = "voices/"+arry[i].path;
+          audio.play();
+        }
+      }
+    },
+    resetorder(){
+      this.orderlist=[];
+    }
   }
 };
 </script>
